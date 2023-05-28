94861d36fa0889fd2a7877fe7ae5cb9ed4c0bcbf
Eduardo-Loiola <128156137+Eduardo-Loiola@users.noreply.github.com>
2023-05-28 18:02:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-número = int(input("Digite um número: "))
-for i in range(1, número+1):
-    if i**2 <= número:
-        print(i**2)
-    else:
-        break
+num_quadrados_perfeitos = int(input("Digite um número: "))
+i = 1
+contagem = 0
+while contagem < num_quadrados_perfeitos:
+    if (int(i ** 0.5)) ** 2 == i:
+        print(i)
+        contagem += 1
+    i += 1
